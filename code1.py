46616d947bfe6a16578435820bdfa8d41041f758
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-08-23 10:32:21 +0300

    Implement delivery

--- code1.py
+++ code1.py
@@ -73,6 +73,9 @@ def clearTables():
     
 def makeFood(food):
     if food == "caliroll":
+        foodOnHand['rice'] -= 1 
+        foodOnHand['nori'] -= 1 
+        foodOnHand['roe'] -= 1  
         pyautogui.click(Cord.f_rice)  # 1 rice
         pyautogui.click(Cord.f_nori)  # 1 nori
         pyautogui.click(Cord.f_roe)  # 1 roe
@@ -80,12 +83,17 @@ def makeFood(food):
         time.sleep(1.5)
     
     elif food == "onigiri":
+        foodOnHand['rice'] -= 2
+        foodOnHand['nori'] -= 1 
         pyautogui.click(Cord.f_rice, clicks=2, interval=0.1) # 2 rice
         pyautogui.click(Cord.f_nori)  # 1 nori
         foldMat()
         time.sleep(1.5)
         
     elif food == "gunkan":
+        foodOnHand['rice'] -= 1 
+        foodOnHand['nori'] -= 1 
+        foodOnHand['roe'] -= 2
         pyautogui.click(Cord.f_rice)  # 1 rice
         pyautogui.click(Cord.f_nori)  # 1 nori
         pyautogui.click(Cord.f_roe, clicks=2, interval=0.1)  # 2 roe
@@ -113,22 +121,77 @@ def buyFood(food):
         pyautogui.click(Cord.menu_rice)
         time.sleep(0.1)
         s = screenGrab()
-        if s.getpixel(Cord.buy_rice) != (224, 216, 206, 255):
-            print("Rice is available")
-            pyautogui.click(Cord.buy_rice)
-            time.sleep(0.1)
-            pyautogui.click(Cord.delivery_norm)
-            time.sleep(2.5)
-        else:
+        if s.getpixel(Cord.buy_rice) != (224, 216, 206, 255): 
             print("Rice is NOT available")
             pyautogui.click(Cord.t_exit)
             time.sleep(1)
             buyFood(food)
+        else:
+            print("Rice is available")
+            pyautogui.click(Cord.buy_rice)
+            time.sleep(0.1)
+            pyautogui.click(Cord.delivery_norm)
+            foodOnHand['rice'] += 10
+            time.sleep(2.5)
+
             
+    if food == "nori":
+        pyautogui.click(Cord.phone)
+        time.sleep(0.1)
+        pyautogui.click(Cord.menu_toppings)
+        time.sleep(0.1)
+        s = screenGrab()
+        print("Test")
+        time.sleep(0.1)
+        if s.getpixel(Cord.t_nori) != (0, 0, 0, 255):
+            print("Nori is NOT available")
+            pyautogui.click(Cord.t_exit)
+            time.sleep(1)
+            buyFood(food)
+        else:
+            print("Nori is available")
+            pyautogui.click(Cord.t_nori)
+            time.sleep(0.1)
+            pyautogui.click(Cord.delivery_norm)
+            foodOnHand['nori'] += 10
+            time.sleep(2.5)
+        
+        
+    if food == "roe":
+        pyautogui.click(Cord.phone)
+        time.sleep(0.1)
+        pyautogui.click(Cord.menu_toppings)
+        time.sleep(0.1)
+        s = screenGrab()
+        if s.getpixel(Cord.t_roe) != (103, 78, 46, 255):
+            print("Roe is NOT available")
+            pyautogui.click(Cord.t_exit)
+            time.sleep(1)
+            buyFood(food)
+        else:
+            print("Roe is available")
+            pyautogui.click(Cord.t_roe)
+            time.sleep(0.1)
+            pyautogui.click(Cord.delivery_norm)
+            foodOnHand['roe'] += 10
+            time.sleep(2.5)
+                
 
 
+foodOnHand = {'shrimp':5,
+              'rice':10,
+              'nori':10,
+              'roe':10,
+              'salmon':5,
+              'unagi':5}
 
 
+def checkFood():
+    for i, j in foodOnHand.items():
+        if i == 'nori' or i == 'rice' or i == 'roe':
+            if j <= 4:
+                print ('%s is low and needs to be replenished' % i)
+                buyFood(i)
 
 
 
@@ -138,4 +201,5 @@ if __name__ == "__main__":
     # makeFood("onigiri")
     # makeFood("caliroll")
     # makeFood("gunkan")
-    clearTables()
+    buyFood("roe")
+    # clearTables()
