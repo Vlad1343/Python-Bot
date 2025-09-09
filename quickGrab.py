c0eea208adb3ee3f0e5d8106b1d70b62676acfc9
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-09-10 00:19:12 +0300

    Python Bot

--- quickGrab.py
+++ quickGrab.py
@@ -6,10 +6,10 @@ def screenGrab():
     time.sleep(5)  # Wait 5 seconds before taking the screenshot
     
     im = ImageGrab.grab()
-    box = (152, 324, 2248, 1422)  # Define the bounding box (left, upper, right, lower)
+    box = (78, 164, 1124, 719)  # Define the bounding box (left, upper, right, lower)
     im = ImageGrab.grab(box)
-    # im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', 'PNG')
-    im.save(os.path.join(os.getcwd(), 'screenshot.png'), 'PNG')
+    im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', 'PNG')
+    # im.save(os.path.join(os.getcwd(), 'screenshot.png'), 'PNG')
 
 def main():
     screenGrab()
