ea8f8339b1d50d82883251d584575e9ab8a2dc84
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-09-10 12:51:56 +0300

    Add recipes

--- code1.py
+++ code1.py
@@ -4,36 +4,6 @@ import time
 x_pad = 77
 y_pad = 163
 
-def mousePos(cord):
-    """Move the cursor to a position relative to the play area."""
-    x, y = cord
-    pyautogui.moveTo(x_pad + x, y_pad + y)
-
-def startGame():
-    """Automates clicking through the game start menus."""
-
-    print("Switch to the game window. Starting in 5 seconds...")
-    time.sleep(5)  # Wait 5 seconds to give time to switch
-    
-    # Location of "Play" button (double click)
-    pyautogui.click(600, 537, clicks=2, interval=1)
-    time.sleep(5)
-
-    # Location of first menu
-    pyautogui.click(600,398)
-    time.sleep(0.1)
-    
-    # Location of second menu
-    pyautogui.click(584, 623)
-    time.sleep(0.1)
-
-    # Location of third menu
-    pyautogui.click(895, 678)
-    time.sleep(0.1)
-
-    # Location of fourth menu
-    pyautogui.click(595, 595)
-    time.sleep(0.1)
 
 class Cord:
     f_shrimp = (273, 547)
@@ -60,6 +30,35 @@ class Cord:
     delivery_norm = (799, 499)
     
 
+def mousePos(cord):
+    """Move the cursor to a position relative to the play area."""
+    x, y = cord
+    pyautogui.moveTo(x_pad + x, y_pad + y)
+
+def startGame():
+    """Automates clicking through the game start menus."""
+    
+    # Location of "Play" button (double click)
+    pyautogui.click(600, 537, clicks=2, interval=1)
+    time.sleep(5)
+
+    # Location of first menu
+    pyautogui.click(600,398)
+    time.sleep(0.1)
+    
+    # Location of second menu
+    pyautogui.click(584, 623)
+    time.sleep(0.1)
+
+    # Location of third menu
+    pyautogui.click(895, 678)
+    time.sleep(0.1)
+
+    # Location of fourth menu
+    pyautogui.click(595, 595)
+    time.sleep(0.1)
+    
+
 def clearTables():
     pyautogui.click(339,403)
     pyautogui.click(457,403)
@@ -68,6 +67,7 @@ def clearTables():
     pyautogui.click(802,403)
     pyautogui.click(922,403)
     time.sleep(1)
+    
     
 def makeFood(food):
     if food == "caliroll":
@@ -79,7 +79,7 @@ def makeFood(food):
     
     elif food == "onigiri":
         pyautogui.click(Cord.f_rice, clicks=2, interval=0.1) # 2 rice
-        pyautogui.click(Cord.f_shrimp)  # 1 shrimp
+        pyautogui.click(Cord.f_nori)  # 1 nori
         foldMat()
         time.sleep(1.5)
         
@@ -100,10 +100,10 @@ def foldMat():
 
 
 
-
-
-
-
-
 if __name__ == "__main__":
+    print("Switch to the game window. Starting in 2 seconds...")
+    time.sleep(2)  # Wait 2 seconds to give time to switch
+    # makeFood("onigiri")
+    # makeFood("caliroll")
+    # makeFood("gunkan")
     clearTables()
